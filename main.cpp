c2c78b11ecc46eb1c635c493035760448a73a8ff
S5578300 <118068983+S5578300@users.noreply.github.com>
2022-12-03 11:25:41 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 
+using namespace std;
 int main(){
-  return 0;
+    int n=0; cin >> n;
+    int m=(n-1);
+    if ((n, m)== true){
+        cout << "numerp primo";
+    }else if ((n, m)== false){
+        cout << "numero non primo";
+    }
+    return 0;
 }
+
